fe78c77288ff30e7a6f1ef9c3489590a4bd01b02
unwon2600 <unwon2600@gmail.com>
2025-11-13 19:57:39 +0530

    Refactor environment variable loading for API config

--- info.py
+++ info.py
@@ -1,13 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# load .env file
 load_dotenv()
 
-API_ID = int(os.getenv("21930652"))
-API_HASH = os.getenv("6cf4623177849a4e534963d98446792e")
-BOT_TOKEN = os.getenv("7852466875:AAHKWS6s0JVAReFLo6YoFSmKk-hXtds0u0Y")
-MONGO_URI = os.getenv("mongodb+srv://unwon2600_db_user:<@unwonperson2600>@unwon.fgh9aoq.mongodb.net/?appName=unwon")
-OWNER_ID = int(os.getenv("-7310926033"))
-LOG_CHANNEL = int(os.getenv("-1002579386518"))
-FILE_CHANNEL = int(os.getenv("-1002676537585"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
+FILE_CHANNEL = int(os.getenv("FILE_CHANNEL"))
